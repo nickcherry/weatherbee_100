6f2ba70fe8f6c5d1983ec52549d9f43a352f76a7
Nick Cherry <ncherry@cipherhealth.com>
2017-01-08 18:15:03 -0500

    remove unused openDropdown function

--- app/containers/UserInputs/ModeInput.js
+++ app/containers/UserInputs/ModeInput.js
@@ -10,7 +10,7 @@ import Select from './Select';
 export class ModeInput extends React.PureComponent { // eslint-disable-line react/prefer-stateless-function
   render() {
     return (
-      <InputWrapper omitPadding onClick={() => this.props.openDropdown(this.selectEl)}>
+      <InputWrapper omitPadding>
         <Select
           defaultValue={this.props.value}
           innerRef={(el) => this.selectEl = el}
@@ -30,20 +30,12 @@ export class ModeInput extends React.PureComponent { // eslint-disable-line reac
 
 ModeInput.propTypes = {
   name: React.PropTypes.string,
-  openDropdown: React.PropTypes.func,
   value: React.PropTypes.string,
   valueChanged: React.PropTypes.func,
 };
 
 function dispatchToProps(dispatch) {
   return {
-    openDropdown: (selectEl) => {
-      selectEl.focus();
-      selectEl.dispatchEvent(new MouseEvent('click', {
-        view: window,
-        bubbles: false,
-      }));
-    },
     valueChanged: (e, inputName) => dispatch(updateUserInput(inputName, e.target.value)),
   };
 }
