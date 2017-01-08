6fa690f48eb9dda643c4c728774e8994e1f824d3
Nick Cherry <ncherry@cipherhealth.com>
2017-01-08 18:17:36 -0500

    only apply extra bottom padding for safari

--- app/containers/UserInputs/Wrapper.js
+++ app/containers/UserInputs/Wrapper.js
@@ -1,8 +1,9 @@
 import styled from 'styled-components';
+import bowser from 'bowser';
 
 const Wrapper = styled.div`
   margin: 6px 8px 6px;
-  padding-bottom: 6px;
+  padding-bottom: ${bowser.safari ? '6px' : 0};
 `;
 
 export default Wrapper;
