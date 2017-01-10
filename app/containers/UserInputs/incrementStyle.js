c3c654e3b566dd6b34bb4a0b1e7748845d080eee
Nick Cherry <ncherry@cipherhealth.com>
2017-01-09 21:56:37 -0500

    remove increment/decrement gradient

--- app/containers/UserInputs/incrementStyle.js
+++ app/containers/UserInputs/incrementStyle.js
@@ -2,10 +2,7 @@ import styled from 'styled-components';
 import { getRgbaColor, getDepth } from '../../utils/styleHelpers';
 
 const incrementStyle = styled.div`
-  background: ${getRgbaColor('windsor')};
-  background: linear-gradient(to top, ${getRgbaColor('studio')} 0%, ${getRgbaColor('windsor')} 100%);
-  -webkit-background-clip: text;
-  color: ${getRgbaColor('studio')};
+  color: ${getRgbaColor('windsor')};
   cursor: pointer;
   font-size: 40px;
   left: 0;
@@ -13,14 +10,9 @@ const incrementStyle = styled.div`
   position: absolute;
   right: 0;
   text-align: center;
-  -webkit-text-fill-color: transparent;
   transform: scaleX(1.25);
   user-select: none;
   z-index: ${getDepth('userInputIncrementer')};
-  &:active {
-    background: linear-gradient(to top, ${getRgbaColor('windsor')} 0%, ${getRgbaColor('studio')} 100%);
-    -webkit-background-clip: text;
-  }
 `;
 
 export default incrementStyle;
