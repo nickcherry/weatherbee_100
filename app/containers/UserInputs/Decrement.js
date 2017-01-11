4ca7589660f242393478924dd2cbc90916c6dfbc
Nick Cherry <ncherry@cipherhealth.com>
2017-01-10 22:21:42 -0500

    nudge inc/dec buttons

--- app/containers/UserInputs/Decrement.js
+++ app/containers/UserInputs/Decrement.js
@@ -2,9 +2,8 @@ import styled from 'styled-components';
 import incrementStyle from './incrementStyle';
 
 const Decrement = styled(incrementStyle)`
-  left: -10px;
+  left: -8px;
   padding-left: 10px
-  padding-right: 2px;
 `;
 
 export default Decrement;
--- app/containers/UserInputs/Increment.js
+++ app/containers/UserInputs/Increment.js
@@ -2,9 +2,8 @@ import styled from 'styled-components';
 import incrementStyle from './incrementStyle';
 
 const Increment = styled(incrementStyle)`
-  padding-left: 2px;
   padding-right: 10px;
-  right: -10px;
+  right: -8px;
 `;
 
 export default Increment;
