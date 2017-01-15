2bcc3021634c349070dbba33720632180f8f046b
Nick Cherry <ncherry@cipherhealth.com>
2017-01-15 18:36:04 -0500

    load local fonts

--- app/global-styles.js
+++ app/global-styles.js
@@ -1,6 +1,11 @@
 import { injectGlobal } from 'styled-components';
 import { getDefaultFont } from './utils/styleHelpers';
 
+import latoLightLatin from 'fonts/lato_light_latin.woff2';
+import latoLightLatinExt from 'fonts/lato_light_latin_ext.woff2';
+import latoRegularLatin from 'fonts/lato_regular_latin.woff2';
+import latoRegularLatinExt from 'fonts/lato_regular_latin_ext.woff2';
+
 /* eslint no-unused-expressions: 0 */
 injectGlobal`
   html, body {
@@ -22,6 +27,39 @@ injectGlobal`
 
   body.fontLoaded {
     font-family: ${getDefaultFont()}, 'Helvetica Neue', Helvetica, Arial, sans-serif;
+  }
+
+  /* latin-ext */
+  @font-face {
+    font-family: 'Lato';
+    font-style: normal;
+    font-weight: 300;
+    src: local('Lato Light'), local('Lato-Light'), url(${latoLightLatin}) format('woff2');
+    unicode-range: U+0100-024F, U+1E00-1EFF, U+20A0-20AB, U+20AD-20CF, U+2C60-2C7F, U+A720-A7FF;
+  }
+  /* latin */
+  @font-face {
+    font-family: 'Lato';
+    font-style: normal;
+    font-weight: 300;
+    src: local('Lato Light'), local('Lato-Light'), url(${latoLightLatinExt}) format('woff2');
+    unicode-range: U+0000-00FF, U+0131, U+0152-0153, U+02C6, U+02DA, U+02DC, U+2000-206F, U+2074, U+20AC, U+2212, U+2215;
+  }
+  /* latin-ext */
+  @font-face {
+    font-family: 'Lato';
+    font-style: normal;
+    font-weight: 400;
+    src: local('Lato Regular'), local('Lato-Regular'), url(${latoRegularLatin}) format('woff2');
+    unicode-range: U+0100-024F, U+1E00-1EFF, U+20A0-20AB, U+20AD-20CF, U+2C60-2C7F, U+A720-A7FF;
+  }
+  /* latin */
+  @font-face {
+    font-family: 'Lato';
+    font-style: normal;
+    font-weight: 400;
+    src: local('Lato Regular'), local('Lato-Regular'), url(${latoRegularLatinExt}) format('woff2');
+    unicode-range: U+0000-00FF, U+0131, U+0152-0153, U+02C6, U+02DA, U+02DC, U+2000-206F, U+2074, U+20AC, U+2212, U+2215;
   }
 
   #app {
