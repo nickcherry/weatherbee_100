0018cc607b3fcc18702db295f8c13b57d339ee6c
Nick Cherry <ncherry@cipherhealth.com>
2017-01-22 12:19:31 -0500

    use userInputs in placeholder calculations

--- app/containers/Ventilator/calculators/patientFeedback/peep.js
+++ app/containers/Ventilator/calculators/patientFeedback/peep.js
@@ -1,3 +1,7 @@
-export default function calc() {
-  return parseInt(Math.random() * 100, 10);
+export default function calc(time, userInputs) {
+  // Nonsensical Placeholder
+  const baseRandomness = Math.random() * 100;
+  const aSprinkeOfTime = (time % 60) / 100;
+  const aDashOfUserInput = (userInputs.freq || 1) / 10;
+  return parseInt(baseRandomness + aSprinkeOfTime, aDashOfUserInput, 10);
 }
--- app/containers/Ventilator/calculators/waveforms/volume.js
+++ app/containers/Ventilator/calculators/waveforms/volume.js
@@ -1,3 +1,4 @@
-export default function calc(time) {
-  return (Math.sin(time) * 50) + 50;
+export default function calc(time, userInputs) {
+  // Nonsensical Placeholder
+  return (Math.sin(time) * 50) + 50 + ((userInputs.freq || 1) / 1000);
 }
